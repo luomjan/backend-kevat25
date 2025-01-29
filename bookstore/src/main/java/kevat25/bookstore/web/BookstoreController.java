ab3a984ba3e870fce02d806ba87365d130980eb0
Nerilka <jamansik@hotmail.com>
2025-01-29 20:39:05 +0200

    lisätty edit ominaisuus

--- bookstore/src/main/java/kevat25/bookstore/web/BookstoreController.java
+++ bookstore/src/main/java/kevat25/bookstore/web/BookstoreController.java
@@ -42,4 +42,16 @@ public class BookstoreController {
         return "redirect:../booklist";
     }
 
+    @RequestMapping(value = "/edit/{id}", method = RequestMethod.GET)
+    public String editBook(@PathVariable("id") Long bookId, Model model) {
+        model.addAttribute("book", repository.findById(bookId));
+        return "editbook";
+    }
+
+    @RequestMapping(value = "/update", method = RequestMethod.POST)
+    public String updateBook(Book book) {
+        repository.save(book);
+        return "redirect:/booklist";
+    }
+
 }
